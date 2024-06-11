b4bfab7fa3edb9d1ae8458d8e084c90f05393f17
William <will513r@gmail.com>
2024-06-11 13:26:15 -0400

    working

---
+++ problem-set-1/practice.js
@@ -0,0 +1,21 @@
+//  Multiplication
+
+// Write a function to find the product (multiplication) of two given numbers only using addition. For example, `multiple(2, 5)` should result in 10.
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+
+// ## Modulo
+
+// Write a function that calculates the modulo (remainder - usually `%`) of a number divided by another number without using your language's modulo operator itself. For example, `mod(137, 10)` should result in 7 and `mod(15, 4)` should result in 1.
+
+
+function modulo(a, b) {
+  console.log(a % b);
+}
+
+// ## Sum
+
+// Write a program to calculate the sum of the digits of the provided number using a loop. For example, `sumOfDigits(25)` should result in 7 and `sumOfDigits(624)` should result in 12.
+
